b3412e0dfa74f9cad2255ab64eeaf9c585b36ba9
Qkite <yeonjigu4@gmail.com>
2022-12-02 14:12:03 +0900

    Encoded password does not look like BCrypt 에러 -> matches 자리 제대로 잡기

--- src/main/java/com/example/springsecurityexcercise/service/UserService.java
+++ src/main/java/com/example/springsecurityexcercise/service/UserService.java
@@ -5,16 +5,24 @@ import com.example.springsecurityexcercise.domain.dto.UserJoinRequest;
 import com.example.springsecurityexcercise.exception.ErrorCode;
 import com.example.springsecurityexcercise.exception.HospitalException;
 import com.example.springsecurityexcercise.repository.UserRepository;
+import com.example.springsecurityexcercise.utils.JwtTokenUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Value("${jwt.token.secret}")
+    private String key;
+    private Long expireTimeMs = 1000*60*60L;
 
     public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
@@ -42,10 +50,15 @@ public class UserService {
     public String login(String userName, String password) {
 
         User selectedUser = userRepository.findByUserName(userName).orElseThrow(() -> new HospitalException(ErrorCode.NOT_FOUND_USER_NAME, "아이디를 찾을 수 없습니다."));
+        
 
-        if(!bCryptPasswordEncoder.matches(selectedUser.getPassword(), password)){
+        if(!bCryptPasswordEncoder.matches(password, selectedUser.getPassword())){
+            
+            // matches(encoding하지 않은 pw, encoding한 pw) 자리 잘 맞추기
             throw new HospitalException(ErrorCode.INVALID_PASSWORD, "비밀번호를 잘 못 입력하셨습니다.");
         }
+
+        String token = JwtTokenUtil.createToken(selectedUser.getUserName(), key, expireTimeMs);
 
         return "token";
 
---
+++ src/main/java/com/example/springsecurityexcercise/utils/JwtTokenUtil.java
@@ -0,0 +1,15 @@
+package com.example.springsecurityexcercise.utils;
+
+import io.jsonwebtoken.Claims;
+
+public class JwtTokenUtil {
+
+    public static String createToken(String userName, String key, long expireTime){
+        Claims claims =
+
+
+        return
+
+
+    }
+}
